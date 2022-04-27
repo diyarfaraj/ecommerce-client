cb37743ed50a7a08dd73ed4db7a1ea56ef83d67e
Diyar Faraj <diyar.faraj@gmail.com>
2022-04-27 13:05:42 +0200

    typescript type added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
+import { useState, useEffect } from "react";
+import { Product } from './product';
+
 
 function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('https://localhost:44337/api/products')
+      .then(res => res.json())
+      .then(data=> setProducts(data))
+
+  }, [])
+  
+
+  function addProduct(){
+    setProducts(prevState=> [...prevState, {
+      id: prevState.length+1,
+      name: "product"+(prevState.length+1), 
+      price:12.00,
+      description: "descrption",
+      imgUrl:"sdfsdf",
+      quantityInStock:23,
+      type:"sfds",
+      brand:"brnad"
+    }]);
+  }
+
   return (
     <div>
      <h1>Kochi Stockholm</h1>
+     {products.map(product => (
+       <p key={product.id}>{product.name} - {product.price}</p>
+     ))}
+     <button onClick={addProduct} >click me</button>
     </div>
   );
 }
---
+++ src/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    imgUrl: string;
+    type: string;
+    brand: string;
+    quantityInStock: number;
+}
