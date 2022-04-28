1780b9f946f31031830a86716cbdd9e9c29f2b27
Diyar Faraj <diyar.faraj@gmail.com>
2022-04-28 15:44:02 +0200

    add router links

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -7,6 +7,11 @@ import {
 import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
 import { useState } from "react";
+import { Route } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import AboutPage from "../../features/about/AboutPage";
+import ContactPage from "../../features/contact/ContactPage";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -28,7 +33,11 @@ function App() {
       <CssBaseline />
       <Header makeItDark={makeItDark} />
       <Container>
-        <Catalog />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/catalog" component={Catalog} />
+        <Route path="/catalog/:id" component={ProductDetails} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/contact" component={ContactPage} />
       </Container>
     </ThemeProvider>
   );
---
+++ src/features/catalog/ProductDetails.tsx
@@ -0,0 +1,5 @@
+import { Typography } from "@mui/material";
+
+export default function ProductDetails() {
+  return <Typography>ProductDetails Page</Typography>;
+}
