90fa28e64d91c7dd2bbbde0d97e13a1a4fc68239
Diyar Faraj <diyar.faraj@gmail.com>
2022-06-12 22:21:31 +0200

    basket item and its agent calls added

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -16,6 +16,7 @@ import ServerError from "../errors/ServerError";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import NotFound from "../errors/NotFound";
+import BasketPage from "../../features/basket/BasketPage";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -45,6 +46,7 @@ function App() {
           <Route path="/about" component={AboutPage} />
           <Route path="/contact" component={ContactPage} />
           <Route path="/server-error" component={ServerError} />
+          <Route path="/basket" component={BasketPage} />
           <Route component={NotFound} />
         </Switch>
       </Container>
