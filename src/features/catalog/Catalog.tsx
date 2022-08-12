fa334730bb4e0b8e39966788ad6e98c8985b3181
Diyar Faraj <diyar.faraj@gmail.com>
2022-08-12 23:50:41 +0200

    logic for paging sorting and filtering added

--- src/features/catalog/Catalog.tsx
+++ src/features/catalog/Catalog.tsx
@@ -8,14 +8,19 @@ import {
 } from "./catalogSlice";
 import { useAppSelector, useAppDispatch } from "../../app/store/configureStore";
 import {
+  Box,
+  Checkbox,
   FormControl,
   FormControlLabel,
+  FormGroup,
   FormLabel,
   Grid,
+  Pagination,
   Paper,
   Radio,
   RadioGroup,
   TextField,
+  Typography,
 } from "@mui/material";
 import { Label } from "@mui/icons-material";
 
@@ -27,9 +32,8 @@ const sortOptions = [
 
 export default function Catalog() {
   const products = useAppSelector(productSelectors.selectAll);
-  const { productsLoaded, status, filtersLoaded } = useAppSelector(
-    (state) => state.catalog
-  );
+  const { productsLoaded, status, filtersLoaded, brands, types } =
+    useAppSelector((state) => state.catalog);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -60,14 +64,46 @@ export default function Catalog() {
                   value={value}
                   control={<Radio />}
                   label={label}
+                  key={value}
                 />
               ))}
             </RadioGroup>
           </FormControl>
         </Paper>
+
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <FormGroup>
+            {brands.map((brand) => (
+              <FormControlLabel
+                control={<Checkbox />}
+                label={brand}
+                key={brand}
+              />
+            ))}
+          </FormGroup>
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <FormGroup>
+            {types.map((type) => (
+              <FormControlLabel
+                control={<Checkbox />}
+                label={type}
+                key={type}
+              />
+            ))}
+          </FormGroup>
+        </Paper>
       </Grid>
       <Grid item xs={9}>
         <ProductList products={products} />
+      </Grid>
+
+      <Grid item xs={3} />
+      <Grid item xs={9}>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Typography>dispalying 1-6 of 20 item</Typography>
+          <Pagination color="secondary" size="large" count={10} page={2} />
+        </Box>
       </Grid>
     </Grid>
   );
--- src/features/catalog/catalogSlice.ts
+++ src/features/catalog/catalogSlice.ts
@@ -3,9 +3,18 @@ import {
   createAsyncThunk,
   createSlice,
 } from "@reduxjs/toolkit";
-import { Product } from "../../app/models/product";
+import { Product, ProductParams } from "../../app/models/product";
 import agent from "../../app/api/agent";
 import { RootState } from "../../app/store/configureStore";
+
+interface CatalogState {
+  productsLoaded: boolean;
+  filtersLoaded: boolean;
+  status: string;
+  brands: string[];
+  types: string[];
+  productParams: ProductParams;
+}
 
 const productAdapter = createEntityAdapter<Product>();
 
@@ -42,17 +51,34 @@ export const fetchFilters = createAsyncThunk(
   }
 );
 
+function initParams() {
+  return {
+    pageNumber: 1,
+    pageSize: 6,
+    orderBy: "name",
+  };
+}
+
 export const catalogSlice = createSlice({
   name: "catalog",
-  initialState: productAdapter.getInitialState({
+  initialState: productAdapter.getInitialState<CatalogState>({
     productsLoaded: false,
     filtersLoaded: false,
     status: "idle",
     brands: [],
     types: [],
+    productParams: initParams(),
   }),
 
-  reducers: {},
+  reducers: {
+    setProductParams: (state, action) => {
+      state.productsLoaded = false;
+      state.productParams = { ...state.productParams, ...action.payload };
+    },
+    resetProductParams: (state) => {
+      state.productParams = initParams();
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(fetchProductsAsync.pending, (state) => {
       state.status = "pendingFetchProducts";
@@ -97,3 +123,5 @@ export const catalogSlice = createSlice({
 export const productSelectors = productAdapter.getSelectors(
   (state: RootState) => state.catalog
 );
+
+export const { setProductParams, resetProductParams } = catalogSlice.actions;
