865fa80b1ba26e222bd536d04e395422b02afe80
Diyar Faraj <diyar.faraj@gmail.com>
2022-08-18 23:05:37 +0200

    add filter component

---
+++ src/app/components/CheckBoxButtons.tsx
@@ -0,0 +1,17 @@
+import { FormGroup, FormControlLabel, Checkbox } from "@mui/material";
+
+interface Props {
+  items: string[];
+  checked: string[];
+  onChange: (items: string[]) => void;
+}
+
+export default function CheckBoxbButtons() {
+  return (
+    <FormGroup>
+      {brands.map((brand) => (
+        <FormControlLabel control={<Checkbox />} label={brand} key={brand} />
+      ))}
+    </FormGroup>
+  );
+}
