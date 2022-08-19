8eae5845bdd2001b450d17b50dc9bcadf66b43b8
Diyar Faraj <diyar.faraj@gmail.com>
2022-08-19 23:53:40 +0200

    checked items logic is added

--- src/app/components/CheckBoxButtons.tsx
+++ src/app/components/CheckBoxButtons.tsx
@@ -1,16 +1,27 @@
 import { FormGroup, FormControlLabel, Checkbox } from "@mui/material";
+import { useState } from "react";
 
 interface Props {
   items: string[];
-  checked: string[];
+  checked?: string[];
   onChange: (items: string[]) => void;
 }
 
 export default function CheckBoxbButtons({ items, checked, onChange }: Props) {
+  const [checkedItems, setCheckedItems] = useState(checked || []);
+
+  function handleChecked(value: string) {
+    const currentIndex = checkedItems.findIndex((item) => item == value);
+    let newChecked: string[] = [];
+    if (currentIndex - 1) newChecked = [...checkedItems, value];
+    else newChecked = checkedItems.filter((item) => item !== value);
+    setCheckedItems(newChecked);
+    onChange(newChecked);
+  }
   return (
     <FormGroup>
-      {brands.map((brand) => (
-        <FormControlLabel control={<Checkbox />} label={brand} key={brand} />
+      {items.map((item) => (
+        <FormControlLabel control={<Checkbox />} label={item} key={item} />
       ))}
     </FormGroup>
   );
