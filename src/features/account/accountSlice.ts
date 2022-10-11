253e76c09796abbf538aec3b88f39582d6d66b18
Diyar Faraj <diyar.faraj@gmail.com>
2022-10-11 22:36:53 +0200

    login slice created

--- src/features/account/accountSlice.ts
+++ src/features/account/accountSlice.ts
@@ -1,5 +1,7 @@
 import { User } from "../../app/models/user";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { FieldValues } from "react-hook-form";
+import agent from "../../app/api/agent";
 
 interface AccountState {
   user: User | null;
@@ -8,6 +10,19 @@ interface AccountState {
 const initialState: AccountState = {
   user: null,
 };
+
+export const signInUser = createAsyncThunk<User, { data: FieldValues }>(
+  "account/singInUser",
+  async (data, thunkAPI) => {
+    try {
+      const user = agent.Account.login(data);
+      localStorage.setItem("user", JSON.stringify(user));
+      return user;
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue({ error: error.data });
+    }
+  }
+);
 
 export const accountSlice = createSlice({
   name: "account",
