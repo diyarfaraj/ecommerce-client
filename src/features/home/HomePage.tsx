bbf4895064339a839ce77f8229343edcca4332f0
Diyar Faraj <diyar.faraj@gmail.com>
2022-12-08 21:55:23 +0100

    changed images

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -9,9 +9,9 @@ import "slick-carousel/slick/slick-theme.css";
 const HomePage = () => {
   // Create an array of images to display in the slide show
   const images = [
-    "https://cdn.pixabay.com/photo/2017/10/31/19/05/web-design-2906159_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2017/10/31/19/05/web-design-2906159_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2017/10/31/19/05/web-design-2906159_960_720.jpg",
+    "https://cdn.shopify.com/s/files/1/2351/6719/files/KOCHI4485_2048x.jpg?v=1660673926",
+    "https://cdn.shopify.com/s/files/1/2351/6719/files/KOCHI4342.jpg_webb.jpg?v=1660673868",
+    "https://cdn.shopify.com/s/files/1/2351/6719/files/1_c569f1d0-00fc-42f8-9fc2-06da8d8dd75d_2048x.jpg?v=1659420470",
   ];
 
   // Configure the settings for the slide show
