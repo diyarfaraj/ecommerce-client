d9906453ef237f565b0562d659aeae09d59a9a4e
Diyar Faraj <diyar.faraj@gmail.com>
2022-12-21 23:13:46 +0100

    add test file

---
+++ src/HomePage.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./app/layout/App";
+import HomePage from "./features/home/HomePage";
+
+test("renders the slide show and welcome message", () => {
+  const { getByText, getByAltText } = render(<HomePage />);
+  const welcomeMessage = getByText(/Welcome to our e-commerce site!/i);
+  expect(welcomeMessage).toBeInTheDocument();
+});
